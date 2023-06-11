0d54b93467284b2e8e37cb5794339cd5710f07fd
Yoel <villayoel@gmail.com>
2023-06-11 23:09:22 +0200

    cambios

--- wwwroot/js/destinos/editar-destinos.js
+++ wwwroot/js/destinos/editar-destinos.js
@@ -1,1 +1,16 @@
-﻿
+﻿const foto = document.getElementById('foto');
+
+let display = "none";
+
+foto.addEventListener('click', () => {
+
+	console.log(display);
+	if (display == "none") {
+		document.getElementById('verFoto').style.display = "block";
+		display = "block";
+	} else {
+		document.getElementById('verFoto').style.display = "none";
+		display = "none";
+	}
+
+});
