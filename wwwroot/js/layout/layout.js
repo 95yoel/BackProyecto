04a6c5a52cea85ada22f6176bdab4b13e1bf2a29
Yoel <villayoel@gmail.com>
2023-06-23 02:01:10 +0200

    aligerar vista layout

---
+++ wwwroot/js/layout/layout.js
@@ -0,0 +1,35 @@
+﻿const elementoHora = document.getElementById("hora");
+
+
+function actualizarHora() {
+
+
+    var fecha = new Date();
+    var dia = fecha.getDate();
+    var mes = fecha.getMonth() + 1;
+    var ano = fecha.getFullYear();
+    var horas = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+
+    if (dia < 10) {
+        dia = "0" + dia;
+    }
+    if (mes < 10) {
+        mes = "0" + mes;
+    }
+    if (horas < 10) {
+        horas = "0" + horas;
+    }
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+
+    var horaActual = dia + "/" + mes + "/" + ano + " - " + horas + ":" + minutos + ":" + segundos;
+    elementoHora.textContent = horaActual;
+}
+
+setInterval(actualizarHora, 1000);
